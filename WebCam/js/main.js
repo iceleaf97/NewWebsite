1c6ee3322bcd59732f2080becf0479e3fb400e61
iceleaf97 <iceleaf97@gmai.com>
2016-12-20 18:27:21 +0800

    test0

--- WebCam/js/main.js
+++ WebCam/js/main.js
@@ -1,19 +1,43 @@
-'use strict';
+// 'use strict';
+//
+// var constraints = {
+//   video: true
+// };
+//
+// var video = document.querySelector('video');
+//
+// function handleSuccess(stream) {
+//   window.stream = stream; // stream available to console
+//   video.src = window.URL.createObjectURL(stream);
+// }
+//
+// function handleError(error) {
+//   console.log('navigator.getUserMedia error: ', error);
+// }
+//
+// navigator.mediaDevices.getUserMedia(constraints).
+//     then(handleSuccess).catch(handleError);
+
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 var constraints = {
-  video: true
+    audio: false,
+    video: { facingMode: { exact: "environment" } }
 };
+var video = document.querySelector("video");
 
-var video = document.querySelector('video');
-
-function handleSuccess(stream) {
-  window.stream = stream; // stream available to console
-  video.src = window.URL.createObjectURL(stream);
+function successCallback(stream) {
+    window.stream = stream; // stream available to console
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
 }
 
-function handleError(error) {
-  console.log('navigator.getUserMedia error: ', error);
+function errorCallback(error){
+    console.log("navigator.getUserMedia error: ", error);
 }
 
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
+navigator.getUserMedia(constraints, successCallback, errorCallback);
